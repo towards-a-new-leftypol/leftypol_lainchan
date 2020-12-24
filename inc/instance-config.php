cf315cf58ca93f161fc7fa1ee8adf8cde285797b
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2020-12-23 19:16:53 -0500

    commit local instance-config.php changes

--- inc/instance-config.php
+++ inc/instance-config.php
@@ -6,8 +6,56 @@
 *  Edit this file and not config.php for imageboard configuration.
 *
 *  You can copy values from config.php (defaults) and paste them here.
-*/
+ */
 
+/*
+ * Front page configurations.
+ */
+
+   $config['boards'] = array(
+       array('leftypol',
+             'b',
+             'hobby',
+             'tech',
+             'edu',
+             'games',
+             'anime'
+       ) ,
+      array('gulag')
+   );
+   // Board categories. Only used in the "Categories" theme.
+   $config['categories'] = array(
+      'Leftypol' => array('leftypol',
+             'b',
+             'hobby',
+             'tech',
+             'edu',
+             'games',
+             'anime'
+       ),
+      'Meta' => array('gulag')
+   );
+   // Optional for the Categories theme. This is an array of name => (title, url) groups for categories
+   // with non-board links.
+   $config['custom_categories'] = array(
+      'Links' => array(
+          'New Multitude' => 'https://newmultitude.org/',
+          'Booru image repository' => 'https://lefty.booru.org/',
+          'Leftypedia' => 'https://leftypedia.org/',
+          'Official chat room' => 'https://app.element.io/#/room/!RQxdjfGouwsFHwUzwL:matrix.org',
+          'Rules' => 'rules.html'
+      ),
+      'Learning resources and blogs' => array(
+          'Michael Roberts\' blog' => 'https://thenextrecession.wordpress.com/',
+          'A Critique Of Crisis Theory blog' => 'https://critiqueofcrisistheory.wordpress.com/',
+          'Leftypedia' => 'https://leftypedia.org/',
+          'Marxis Internet Archive' => 'https://www.marxists.org/'
+      ),
+   );
+
+/*
+ * Database and site wide configurations
+ */
 
 	$config['db']['server'] = 'localhost';
 	$config['db']['database'] = 'lainchan';
@@ -38,4 +86,61 @@
 
 	$config['thumb_method'] = 'gm+gifsicle';
 	$config['gnu_md5'] = '1';
+	// $config['update_on_posts'] = true;
 
+
+// Changes made via web editor by "krates" @ Tue, 22 Dec 2020 16:28:45 -0800:
+$config['robot_mute'] = false;
+
+
+// Changes made via web editor by "krates" @ Tue, 22 Dec 2020 16:29:57 -0800:
+$config['max_links'] = 100;
+$config['reply_limit'] = 750;
+
+// Changes made via web editor by "krates" @ Tue, 22 Dec 2020 16:34:13 -0800:
+$config['min_body'] = 0;
+
+
+// Changes made via web editor by "krates" @ Tue, 22 Dec 2020 16:37:13 -0800:
+$config['anti_bump_flood'] = true;
+
+
+// Changes made via web editor by "krates" @ Tue, 22 Dec 2020 16:38:21 -0800:
+$config['delete_time'] = 5;
+
+
+// Changes made via web editor by "krates" @ Tue, 22 Dec 2020 17:20:14 -0800:
+$config['reply_hard_limit'] = 1000;
+
+
+/*
+ * ====================
+ *  Javascript
+ * ====================
+ */
+
+	$config['additional_javascript'][] = 'js/local-time.js';
+	$config['additional_javascript'][] = 'js/auto-reload.js';
+	$config['additional_javascript'][] = 'js/post-hover.js';
+	$config['additional_javascript'][] = 'js/style-select.js';
+
+    $config['additional_javascript'][] = 'js/hide-threads.js';
+    $config['additional_javascript'][] = 'js/hide-images.js';
+    $config['additional_javascript'][] = 'js/show-backlinks.js';
+    $config['additional_javascript'][] = 'js/show-op.js';
+
+    $config['additional_javascript'][] = 'js/jquery-ui.custom.min.js';
+    $config['additional_javascript'][] = 'js/quick-reply.js';
+
+    $config['enable_embedding'] = true;
+
+    $config['youtube_js_html'] = '<div class="video-container" data-video="$2">'.
+    '<a href="https://youtu.be/$2" target="_blank" class="file">'.
+    '<img style="width:255px;height:190px;" src="//img.youtube.com/vi/$2/0.jpg" class="post-image"/>'.
+    '</a></div>';
+
+    $config['embedding'] = array();
+    $config['embedding'][0] = array(
+    '/^https?:\/\/(\w+\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9\-_]{10,11})(&.+)?$/i',
+    $config['youtube_js_html']);
+    $config['additional_javascript'][] = 'js/youtube.js';
