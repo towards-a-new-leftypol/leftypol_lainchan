6aa316138028f9febef34f77403e3d5d3848e433
nonmakina <nonmakina@leftypol.org>
2021-01-04 17:46:50 -0600

    Fixes indenting

--- inc/api.php
+++ inc/api.php
@@ -93,9 +93,9 @@ class Api {
 		$dotPos = strrpos($file->file, '.');
 		$apiPost['ext'] = substr($file->file, $dotPos);
 		$apiPost['tim'] = substr($file->file, 0, $dotPos);
-        if (isset ($file->thumb) && $file->thumb) {                             
-            $apiPost['spoiler'] = $file->thumb === 'spoiler' ? 1 : 0;           
-        }
+		if (isset ($file->thumb) && $file->thumb) {
+			$apiPost['spoiler'] = $file->thumb === 'spoiler' ? 1 : 0;
+ 		}
 		if (isset ($file->hash) && $file->hash) {
 			$apiPost['md5'] = base64_encode(hex2bin($file->hash));
 		}
