a9885c4a9bf25875a4265f0fb032e4c5d86876c6
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2021-04-03 20:19:23 -0400

    Remove reference to non-existant js file in config.php

--- inc/config.php
+++ inc/config.php
@@ -1033,7 +1033,6 @@
     // Additional Javascript files to include on board index and thread pages. See js/ for available scripts.
     $config['additional_javascript'][] = 'js/jquery.min.js';
     $config['additional_javascript'][] = 'js/inline-expanding.js';
-    $config['additional_javascript'][] = 'js/multi_image.js'; // required when using multi file upload
     // $config['additional_javascript'][] = 'js/local-time.js';
 
     // Some scripts require jQuery. Check the comments in script files to see what's needed. When enabling
