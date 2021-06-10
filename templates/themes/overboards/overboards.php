98f804cbf22c03916f8748b204dc9a6c954228c4
discomrade <nice@try.fbi>
2021-06-10 05:25:23 -0200

    Change default overboard subtitles and thread count

--- templates/themes/overboards/overboards.php
+++ templates/themes/overboards/overboards.php
@@ -4,21 +4,21 @@
  * When adding a new board, rebuild this theme. If necessary, reconfigure the catalog theme.
  * Exclude list is space-separated (e.g. 'exclude' => 'b games music' )
  */
-	$thread_limit = 15;
+	$thread_limit = 30;
 
 	// Define list of overboards
 	$overboards_config = array(
 		array(
 			'title' => 'Overboard',
 			'uri' => 'overboard',
-			'subtitle' => 'something something overboard',
+			'subtitle' => '30 most recently bumped threads',
 			'exclude' => '',
 			'thread_limit' => $thread_limit,
 		),
 		array(
 			'title' => 'SFW Overboard',
 			'uri' => 'sfwoverboard',
-			'subtitle' => 'something something sfw overboard',
+			'subtitle' => '30 most recently bumped threads from work-safe boards',
 			'exclude' => 'b',
 			'thread_limit' => $thread_limit,
 		),
