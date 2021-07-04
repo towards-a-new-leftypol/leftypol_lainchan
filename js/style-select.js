6687d9a86398e4c9b2315449a83c969d327ed4de
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2021-07-03 22:26:31 -0400

    retab file

--- js/style-select.js
+++ js/style-select.js
@@ -6,7 +6,7 @@
  *
  * Released under the MIT license
  * Copyright (c) 2013 Michael Save <savetheinternet@tinyboard.org>
- * Copyright (c) 2013-2014 Marcin Łabanowski <marcin@6irc.net> 
+ * Copyright (c) 2013-2014 Marcin Å�abanowski <marcin@6irc.net> 
  *
  * Usage:
  *   $config['additional_javascript'][] = 'js/jquery.min.js';
@@ -15,41 +15,41 @@
  */
 
 $(document).ready(function() {
-	var stylesDiv = $('div.styles');
-	var pages = $('div.pages');
-	var stylesSelect = $('<select></select>').css({float:"none"});
-	var options = [];
-	
-	var i = 1;
-	stylesDiv.children().each(function() {
-		var name = this.innerHTML.replace(/(^\[|\]$)/g, '');
-		var opt = $('<option></option>')
-			.html(name)
-			.val(i);
-		if ($(this).hasClass('selected'))
-			opt.attr('selected', true);
-		options.push ([name.toUpperCase (), opt]);
-		$(this).attr('id', 'style-select-' + i);
-		i++;
-	});
+    var stylesDiv = $('div.styles');
+    var pages = $('div.pages');
+    var stylesSelect = $('<select></select>').css({float:"none"});
+    var options = [];
+    
+    var i = 1;
+    stylesDiv.children().each(function() {
+        var name = this.innerHTML.replace(/(^\[|\]$)/g, '');
+        var opt = $('<option></option>')
+            .html(name)
+            .val(i);
+        if ($(this).hasClass('selected'))
+            opt.attr('selected', true);
+        options.push ([name.toUpperCase (), opt]);
+        $(this).attr('id', 'style-select-' + i);
+        i++;
+    });
 
-	options.sort ((a, b) => {
-		const keya = a [0];
-		const keyb = b [0];
-		if (keya < keyb) { return -1; }
-		if (keya > keyb) { return  1; }
-		return 0;
-	}).forEach (([key, opt]) => {
-		stylesSelect.append(opt);
-	});
-	
-	stylesSelect.change(function() {
-		$('#style-select-' + $(this).val()).click();
-	});
-	
-	stylesDiv.hide()	
-	pages.after(
-		$('<div id="style-select"></div>')
-			.append(_('Select theme: '), stylesSelect)
-	);
+    options.sort ((a, b) => {
+        const keya = a [0];
+        const keyb = b [0];
+        if (keya < keyb) { return -1; }
+        if (keya > keyb) { return  1; }
+        return 0;
+    }).forEach (([key, opt]) => {
+        stylesSelect.append(opt);
+    });
+    
+    stylesSelect.change(function() {
+        $('#style-select-' + $(this).val()).click();
+    });
+    
+    stylesDiv.hide()    
+    pages.after(
+        $('<div id="style-select"></div>')
+            .append(_('Select theme: '), stylesSelect)
+    );
 });
