0934c452e84eef0c4df852c5d974ce89cfd67d52
nonmakina <nonmakina@leftypol.org>
2021-07-11 19:43:07 +0200

    Fix indentation

--- js/webp-fix.js
+++ js/webp-fix.js
@@ -13,25 +13,24 @@ $(document).ready(function() {
 
 	// from: https://stackoverflow.com/a/54120785
 	function testWebP() {
-	    return new Promise(res => {
-	        const webP = new Image();
-	        webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-	        webP.onload = webP.onerror = () => {
-	            res(webP.height === 2);
-	        };        
-	    })
+		return new Promise(res => {
+			const webP = new Image();
+			webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+			webP.onload = webP.onerror = () => {
+				res(webP.height === 2);
+			};        
+		})
 	};
 
 	testWebP().then(hasWebP => {
-
 		if(!hasWebP){
 			$('div.file a').each(function(index){
 				let ext = ["png","jpg", "jpeg"];
 				let file = $(this);
 				let fullSize = file.prop("href");
-			  if (ext.includes(fullSize.split('.').pop())){
-			    file.find('img').prop("src", fullSize);
-			  }
+				if (ext.includes(fullSize.split('.').pop())){
+					file.find('img').prop("src", fullSize);
+			  	}
 			});
 		}
 	});
