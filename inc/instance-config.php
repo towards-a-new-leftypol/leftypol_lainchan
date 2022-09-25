534c5ca9d81888dac9e0c66adb02dddd2e4bfc67
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2022-09-25 00:06:03 -0400

    remove boards we don't actually have

--- inc/instance-config.php
+++ inc/instance-config.php
@@ -13,23 +13,9 @@
  */
 
 $config['boards'] = array(
-  array(
-    'leftypol',
-    'b',
-    'WRK',
-    'hobby',
-    'tech',
-    'edu',
-    'ga',
-    'ent',
-    '777',
-    'posad',
-    'i',
-    'a',
-    'R9K',
-    'dead'
-  ),
-  array('meta')
+	array(
+		'b',
+	)
 );
 
 $config['prepended_foreign_boards'] = array(
@@ -41,23 +27,9 @@ $config['prepended_foreign_boards'] = array(
 
 // Board categories. Only used in the "Categories" theme.
 $config['categories'] = array(
-  'Leftypol' => array(
-    'leftypol',
-    'b',
-    'WRK',
-    'hobby',
-    'tech',
-    'edu',
-    'ga',
-    'ent',
-    '777',
-    'posad',
-    'i',
-    'a',
-    'R9K',
-    'dead'
-  ),
-  'Meta' => array('meta')
+	'Leftypol' => array(
+		'b',
+	)
 );
 
 // Optional for the Categories theme. This is an array of name => (title, url) groups for categories
