e9372e50eeb3f01ff103d65d05eec9df007b8a3e
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2023-12-13 02:23:31 +0000

    Search.php - use FETCH_ASSOC to avoid issues when building Post objects

--- search.php
+++ search.php
@@ -142,7 +142,7 @@
 		}
 
 		$temp = '';
-		while($post = $query->fetch()) {
+		while($post = $query->fetch(PDO::FETCH_ASSOC)) {
 			if(!$post['thread']) {
 				$po = new Thread($post);
 			} else {
