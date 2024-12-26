60d86d3a845dcf00c8c0c25d6d30424636777768
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2024-12-26 07:36:08 +0000

    Fix commit 1c13ec98ba188ec9829d9224b52227dccfefd3b2
    - Unintentionally disabled that piece of code instead of removing adding
      the additional br tag that made double spaces between posts.
    - add the postcontainers instead of post.reply (which is inside this
      container) to preserve the arrows

--- js/expand.js
+++ js/expand.js
@@ -28,17 +28,15 @@ $(document).ready(function(){
 					context: document.body,
 					success: function(data) {
 						var last_expanded = false;
-						$(data).find('div.post.reply').each(function() {
+						$(data).find('div.postcontainer').each(function() {
 							thread.find('div.hidden').remove();
 							var post_in_doc = thread.find('#' + $(this).attr('id'));
 							if(post_in_doc.length == 0) {
-                                /*
 								if(last_expanded) {
-									$(this).addClass('expanded').insertAfter(last_expanded).before('<br class="expanded">');
+									$(this).addClass('expanded').insertAfter(last_expanded);
 								} else {
-									$(this).addClass('expanded').insertAfter(thread.find('div.post:first')).after('<br class="expanded">');
+									$(this).addClass('expanded').insertAfter(thread.find('div.post:first'));
 								}
-                                */
 								last_expanded = $(this);
 								$(document).trigger('new_post', this);
 							} else {
